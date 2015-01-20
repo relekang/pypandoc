c8b53885d6b79a40399f6434b0f76563fb1fc19f
Rolf Erik Lekang <me@rolflekang.com>
2015-01-20 22:57:50 +0100

    Fix python3 support

--- pypandoc.py
+++ pypandoc.py
@@ -12,11 +12,24 @@ import re
 
 
 def convert(source, to, format=None, extra_args=(), encoding='utf-8'):
-    '''Converts given `source` from `format` `to` another. `source` may be either a file path or a string to be converted. It's possible to pass `extra_args` if needed. In case `format` is not provided, it will try to invert the format based on given `source`.
+    '''Converts given `source` from `format` `to` another. `source` may be
+    either a file path or a string to be converted. It's possible to pass
+    `extra_args` if needed. In case `format` is not provided, it will try
+    to invert the format based on given `source`.
 
-    Raises OSError if pandoc is not found! Make sure it has been installed and is available at path.
+    Raises OSError if pandoc is not found! Make sure it has been installed
+    and is available at path.
     '''
-    return _convert(_read_file, _process_file, source, to, format, extra_args, encoding=encoding)
+    return _convert(
+        _read_file,
+        _process_file,
+        source,
+        to,
+        format,
+        extra_args,
+        encoding=encoding
+    )
+
 
 def _convert(reader, processor, source, to, format=None, extra_args=(), encoding=None):
     source, format = reader(source, format, encoding=encoding)
@@ -37,12 +50,15 @@ def _convert(reader, processor, source, to, format=None, extra_args=(), encoding
     from_formats, to_formats = get_pandoc_formats()
 
     if format not in from_formats:
-        raise RuntimeError('Invalid input format! Expected one of these: ' + ', '.join(from_formats))
+        raise RuntimeError(
+            'Invalid input format! Expected one of these: ' + ', '.join(from_formats)
+        )
 
     if to not in to_formats:
         raise RuntimeError('Invalid to format! Expected one of these: ' + ', '.join(to_formats))
 
     return processor(source, to, format, extra_args)
+
 
 def _read_file(source, format, encoding='utf-8'):
     try:
@@ -57,40 +73,44 @@ def _read_file(source, format, encoding='utf-8'):
 
     return source, format
 
+
 def _process_file(source, to, format, extra_args):
     args = ['pandoc', '--from=' + format, '--to=' + to]
     args.extend(extra_args)
 
     p = subprocess.Popen(
-            args,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE)
+        args,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
 
     try:
         c = p.communicate(source.encode('utf-8'))[0].decode('utf-8')
-    except UnicodeDecodeError, UnicodeEncodeError:
+    except (UnicodeDecodeError, UnicodeEncodeError):
         c = p.communicate(source)[0]
 
     return c
+
 
 def get_pandoc_formats():
     '''
     Dynamic preprocessor for Pandoc formats.
     Return 2 lists. "from_formats" and "to_formats".
-    ''' 
+    '''
     try:
         p = subprocess.Popen(
-                ['pandoc', '-h'],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE)
+            ['pandoc', '-h'],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE
+        )
     except OSError:
         raise OSError("You probably do not have pandoc installed.")
-    
+
     help_text = p.communicate()[0].decode().splitlines(False)
     txt = ' '.join(help_text[1:help_text.index('Options:')])
 
     aux = txt.split('Output formats: ')
-    in_ = re.sub('Input\sformats:\s','',aux[0]).split(',')
-    out = re.sub('\*|\[.*?\]','',aux[1]).split(',')
+    in_ = re.sub('Input\sformats:\s', '', aux[0]).split(',')
+    out = re.sub('\*|\[.*?\]', '', aux[1]).split(',')
 
     return [f.strip() for f in in_], [f.strip() for f in out]
